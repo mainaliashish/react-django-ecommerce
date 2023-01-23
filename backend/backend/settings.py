e514241fdbe55e7c27cade6fdf7535388f0bd888
Ashish Mainali <19223717@brookes.com>
2023-01-23 20:55:21 +0000

    - Update add django cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'base'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
 
-# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    return Response('Hello')
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for product in products:
+        if product['_id'] == pk:
+            product = product
+            break
+    return Response(product)
