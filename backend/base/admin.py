33de330e51d2b948703a5347ee355f86d4979f3a
Ashish Mainali <19223717@brookes.com>
2023-01-23 21:46:29 +0000

    - Update admin file to register models

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import *
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -9,3 +9,7 @@ class ProductAdmin(admin.ModelAdmin):
         model = Product
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
