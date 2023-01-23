11d834c41b9fe31e58063cd69387e4dc35610f53
Ashish Mainali <19223717@brookes.com>
2023-01-23 21:40:28 +0000

    - Update add order item and migrations

--- backend/base/models.py
+++ backend/base/models.py
@@ -54,3 +54,17 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.createdAt)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
